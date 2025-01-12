acf772288efed3bfb1461ee8a1fe1ec32db1645c
Abdullah Al-Karim Amrullah <abdullahalkarimamrullah01@gmail.com>
2025-01-12 15:57:27 +0800

    fix: add ca certificate

--- database.js
+++ database.js
@@ -1,11 +1,15 @@
-const postgres = require('postgres')
+const postgres = require("postgres")
 
 const sql = postgres({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
-  password: process.env.DB_PASS
+  password: process.env.DB_PASS,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.DB_CA_CERT,
+  },
 }) // will use psql environment variables
 
 module.exports = sql
